9ae5caff072bf1ca3e408da39fcfa056b09b1b6e
ihkwon <ihkwon@nethru.co.kr>
2018-11-07 16:18:43 +0900

    version 4

--- VideoRental/src/test/java/step1/CustomerTest.java
+++ VideoRental/src/test/java/step1/CustomerTest.java
@@ -8,8 +8,10 @@ public class CustomerTest {
 
     public static final int TWO_DAYS = 2;
     public static final int THREE_DAYS = 3;
+    public static final int FOUR_DAYS = 4;
+    public static final int ONE_DAY = 1;
 
-    String name = null;
+    String name = "CUSTOMER_NAME_NOT_IMPORTANT";
     Customer customer = new Customer(name);
 
     @Test
@@ -19,7 +21,7 @@ public class CustomerTest {
         String statement = customer.statement();
 
         // Assert - Then - Post-Condition
-        assertEquals("Rental Record for null\n" +
+        assertEquals("Rental Record for CUSTOMER_NAME_NOT_IMPORTANT\n" +
                 "Amount owed is 0.0\n" +
                 "You earned 0 frequent renter pointers", statement);
     }
@@ -30,8 +32,8 @@ public class CustomerTest {
 
         String statement = customer.statement();
 
-        assertEquals("Rental Record for null\n" +
-                "\t2.0(null)\n" +
+        assertEquals("Rental Record for CUSTOMER_NAME_NOT_IMPORTANT\n" +
+                "\t2.0(TITLE_NOT_IMPORTANT)\n" +
                 "Amount owed is 2.0\n" +
                 "You earned 1 frequent renter pointers", statement);
     }
@@ -42,8 +44,8 @@ public class CustomerTest {
 
         String statement = customer.statement();
 
-        assertEquals("Rental Record for null\n" +
-                "\t3.5(null)\n" +
+        assertEquals("Rental Record for CUSTOMER_NAME_NOT_IMPORTANT\n" +
+                "\t3.5(TITLE_NOT_IMPORTANT)\n" +
                 "Amount owed is 3.5\n" +
                 "You earned 1 frequent renter pointers", statement);
     }
@@ -54,8 +56,8 @@ public class CustomerTest {
 
         String statement = customer.statement();
 
-        assertEquals("Rental Record for null\n" +
-                "\t6.0(null)\n" +
+        assertEquals("Rental Record for CUSTOMER_NAME_NOT_IMPORTANT\n" +
+                "\t6.0(TITLE_NOT_IMPORTANT)\n" +
                 "Amount owed is 6.0\n" +
                 "You earned 2 frequent renter pointers", statement);
     }
@@ -66,32 +68,32 @@ public class CustomerTest {
 
         String statement = customer.statement();
 
-        assertEquals("Rental Record for null\n" +
-                "\t3.0(null)\n" +
+        assertEquals("Rental Record for CUSTOMER_NAME_NOT_IMPORTANT\n" +
+                "\t3.0(TITLE_NOT_IMPORTANT)\n" +
                 "Amount owed is 3.0\n" +
                 "You earned 1 frequent renter pointers", statement);
     }
 
     @Test
     public void statement_for_childrensMovie_when_rents_less_than_4_days() {
-        customer.addRental(createRentalFor(Movie.CHILDRENS, 3));
+        customer.addRental(createRentalFor(Movie.CHILDRENS, THREE_DAYS));
 
         String statement = customer.statement();
 
-        assertEquals("Rental Record for null\n" +
-                "\t1.5(null)\n" +
+        assertEquals("Rental Record for CUSTOMER_NAME_NOT_IMPORTANT\n" +
+                "\t1.5(TITLE_NOT_IMPORTANT)\n" +
                 "Amount owed is 1.5\n" +
                 "You earned 1 frequent renter pointers", statement);
     }
 
     @Test
     public void statement_for_childrensMovie_when_rents_more_than_3_days() {
-        customer.addRental(createRentalFor(Movie.CHILDRENS, 4));
+        customer.addRental(createRentalFor(Movie.CHILDRENS, FOUR_DAYS));
 
         String statement = customer.statement();
 
-        assertEquals("Rental Record for null\n" +
-                "\t3.0(null)\n" +
+        assertEquals("Rental Record for CUSTOMER_NAME_NOT_IMPORTANT\n" +
+                "\t3.0(TITLE_NOT_IMPORTANT)\n" +
                 "Amount owed is 3.0\n" +
                 "You earned 1 frequent renter pointers", statement);
     }
@@ -101,21 +103,21 @@ public class CustomerTest {
         customer.addRental(createRentalFor(Movie.REGULAR, TWO_DAYS));
         customer.addRental(createRentalFor(Movie.REGULAR, THREE_DAYS));
 
-        customer.addRental(createRentalFor(Movie.NEW_RELEASE, 1));
+        customer.addRental(createRentalFor(Movie.NEW_RELEASE, ONE_DAY));
         customer.addRental(createRentalFor(Movie.NEW_RELEASE, TWO_DAYS));
 
-        customer.addRental(createRentalFor(Movie.CHILDRENS, 3));
-        customer.addRental(createRentalFor(Movie.CHILDRENS, 4));
+        customer.addRental(createRentalFor(Movie.CHILDRENS, THREE_DAYS));
+        customer.addRental(createRentalFor(Movie.CHILDRENS, FOUR_DAYS));
 
         String statement = customer.statement();
 
-        assertEquals("Rental Record for null\n" +
-                "\t2.0(null)\n" +
-                "\t3.5(null)\n" +
-                "\t3.0(null)\n" +
-                "\t6.0(null)\n" +
-                "\t1.5(null)\n" +
-                "\t3.0(null)\n" +
+        assertEquals("Rental Record for CUSTOMER_NAME_NOT_IMPORTANT\n" +
+                "\t2.0(TITLE_NOT_IMPORTANT)\n" +
+                "\t3.5(TITLE_NOT_IMPORTANT)\n" +
+                "\t3.0(TITLE_NOT_IMPORTANT)\n" +
+                "\t6.0(TITLE_NOT_IMPORTANT)\n" +
+                "\t1.5(TITLE_NOT_IMPORTANT)\n" +
+                "\t3.0(TITLE_NOT_IMPORTANT)\n" +
                 "Amount owed is 19.0\n" +
                 "You earned 7 frequent renter pointers", statement);
     }
@@ -125,7 +127,7 @@ public class CustomerTest {
     }
 
     private Rental createRentalFor(int priceCode, int daysRented) {
-        String title = null;
+        String title = "TITLE_NOT_IMPORTANT";
         Movie movie = new Movie(title, priceCode);
         return new Rental(movie, daysRented);
     }
