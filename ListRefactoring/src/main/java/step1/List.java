fcd52e597df1f3121903dde49fee32aebe9fa77a
Junghoon Park <mugenjh@gmail.com>
2018-11-12 13:03:48 +0900

    List refactoring

--- ListRefactoring/src/main/java/step1/List.java
+++ ListRefactoring/src/main/java/step1/List.java
@@ -1,26 +1,43 @@
 package step1;
 
+
 public class List {
-	private Object[] elements = new Object[10];
+
+	public static final int INITIAL_STORE_SIZE = 10;
+	public static final int STORE_SIZE_INCREMENT = 10;
+	private Object[] elementStore = new Object[INITIAL_STORE_SIZE];
 	private boolean readOnly;
 	private int size = 0;
 
 	public void add(Object element) {
-		if (!readOnly) {
-			int newSize = size + 1;
-			
-			if ( newSize > elements.length) {
-				Object[] newElements = new Object[elements.length + 10];
-				for (int i = 0; i < size; i++) {
-					newElements[i] = elements[i];
-				}
-
-				elements = newElements;
-			}
-
-			elements[size] = element;
-			size++;
+		if (readOnly) {
+			return;
 		}
+
+		if (isElementStoreFull()) {
+			increaseElementStore();
+		}
+
+		addElementAtEnd(element);
+	}
+
+	private void addElementAtEnd(Object element) {
+		elementStore[size] = element;
+		size++;
+	}
+
+	private boolean isElementStoreFull() {
+		return size + 1 > elementStore.length;
+	}
+
+	private void increaseElementStore() {
+		Object[] newElementStore =
+			new Object[elementStore.length + STORE_SIZE_INCREMENT];
+		for (int i = 0; i < size; i++) {
+			newElementStore[i] = elementStore[i];
+		}
+
+		elementStore = newElementStore;
 	}
 
 	public int size() {
@@ -28,6 +45,6 @@ public class List {
 	}
 
 	public Object get(int index) {
-		return elements[index];
+		return elementStore[index];
 	}
 }
